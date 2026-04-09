6b98379ca65992178e9e20ee43b24a6a33cad15d
Somya Rawat <somyarawat2005@gmail.com>
2026-04-09 12:36:43 +0530

    Convert height from cm to inches and feet

---
+++ core-programming/core-programming-basic/programming-elements/level-1/HeightConversion.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class HeightConversion {
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter height in cm: ");
+        double cm = sc.nextDouble();
+
+        double inches = cm / 2.54;
+        double feet = inches / 12;
+
+        System.out.println("Feet: " + feet + " Inches: " + inches);
+        sc.close();
+    }
+}
