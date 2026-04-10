1bf675ea3dcb4fc55f5d56988b62fcecf188a56a
Somya Rawat <somyarawat2005@gmail.com>
2026-04-10 16:36:49 +0530

    Perform Different Operations on Double Data Types Values

---
+++ core-programming/core-programming-basic/programming-elements/level-2/DoubleExpressions.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class DoubleExpressions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Integer Values: ");
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        double c = sc.nextInt();
+
+        System.out.println(a + b * c);
+        System.out.println(a * b + c);
+        System.out.println(c + a / b);
+        System.out.println(a % b + c);
+        
+    }
+}
