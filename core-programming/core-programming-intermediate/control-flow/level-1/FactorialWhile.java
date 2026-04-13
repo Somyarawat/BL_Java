2e2937f2e8c7ff7452d7d208619b00051ef025d5
Somya Rawat <somyarawat2005@gmail.com>
2026-04-13 14:36:43 +0530

    Calculate Factorial Using While Loop

---
+++ core-programming/core-programming-intermediate/control-flow/level-1/FactorialWhile.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class FactorialWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+        int fact = 1;
+        int i = 1;
+
+        while(i <= n) {
+            fact *= i;
+            i ++;
+        }
+        System.out.println(fact);
+    }
+}
