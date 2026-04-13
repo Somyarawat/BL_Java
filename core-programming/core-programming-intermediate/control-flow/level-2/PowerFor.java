b5fd39ff4e6207ddfa3ebe8d4c171681eb2189bc
Somya Rawat <somyarawat2005@gmail.com>
2026-04-14 00:53:06 +0530

    Calculate Power Using For Loop

---
+++ core-programming/core-programming-intermediate/control-flow/level-2/PowerFor.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class PowerFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number: ");
+        int number = sc.nextInt();
+        System.out.print("Enter Power: ");
+        int power = sc.nextInt();
+        int result = 1;
+
+        if(number > 0) {
+            for (int i = 1; i <= power; i++){
+                result *= number;
+            }
+        }
+        System.out.println(result);
+    }
+}
