1a425dd6e6aca3a1c59cbf5b4e0a8cdcdc614013
Somya Rawat <somyarawat2005@gmail.com>
2026-04-14 11:08:08 +0530

    Leap Year Program With Single If Condition

---
+++ core-programming/core-programming-intermediate/control-flow/level-3/LeapYearSingle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class LeapYearSingle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Year: ");
+        int year = sc.nextInt();
+
+        if(year >= 1582 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)) {
+            System.out.println("Leap Year");
+        }
+        else {
+            System.out.println("Not a Leap Year");
+        }
+    }
+}
