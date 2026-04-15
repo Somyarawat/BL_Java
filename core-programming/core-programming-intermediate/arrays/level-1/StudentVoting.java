d8b1a07eaa3008c45636638e96ed07c6c1970b60
Somya Rawat <somyarawat2005@gmail.com>
2026-04-15 11:37:30 +0530

    Check the age of 10 students in a class whether they can vote or not

---
+++ core-programming/core-programming-intermediate/arrays/level-1/StudentVoting.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class StudentVoting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] ages = new int[10];
+
+        System.out.print("Enter Elements of Array: ");
+        for(int i = 0; i < ages.length; i++){
+            ages[i] = sc.nextInt();
+
+            if (ages[i] < 0) {
+                System.out.println("Invalid Age");
+            }
+            else if (ages[i] >= 18) {
+                System.out.println("The student with the age " + ages[i] + " can vote.");
+            }
+            else {
+                System.out.println("The student with the age " + ages[i] + " cannot vote");
+            }
+        }
+    }
+}
