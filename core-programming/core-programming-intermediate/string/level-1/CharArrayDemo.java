7c80a366fd2b78028bb09d9427f5df2951c7abeb
Somya Rawat <somyarawat2005@gmail.com>
2026-04-17 00:07:11 +0530

    Return all the character in a string

---
+++ core-programming/core-programming-intermediate/string/level-1/CharArrayDemo.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class CharArrayDemo {
+    public static char[] convert(String str) {
+        char[] arr = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            arr[i] = str.charAt(i);
+        }
+        return arr;
+    }
+
+    public static boolean compare(char[] a, char[] b) {
+        if (a.length != b.length) return false;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+
+        char[] manual = convert(text);
+        char[] builtIn = text.toCharArray();
+
+        System.out.println("Same? " + compare(manual, builtIn));
+    }
+}
