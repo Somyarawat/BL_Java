99570b24f3ce6d3aa52da3752b20e1606d8380be
Somya Rawat <somyarawat2005@gmail.com>
2026-04-17 23:25:03 +0530

    Demonstrate NumberFormatException

---
+++ core-programming/core-programming-intermediate/string/level-1/NumberFormat.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class NumberFormatDemo {
+
+    public static void generate(String text) {
+        int num = Integer.parseInt(text);
+        System.out.println(num);
+    }
+
+    public static void handle(String text) {
+        try {
+            int num = Integer.parseInt(text);
+            System.out.println(num);
+        } catch (NumberFormatException e) {
+            System.out.println("Handled NumberFormatException");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        handle(input);
+    }
+}
