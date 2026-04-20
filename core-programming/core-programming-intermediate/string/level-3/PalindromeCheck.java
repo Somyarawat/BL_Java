5b93dba1996caa7d51b27ab6df880373689d8030
Somya Rawat <somyarawat2005@gmail.com>
2026-04-20 17:11:35 +0530

    Check the string is palindrome or not

---
+++ core-programming/core-programming-intermediate/string/level-3/PalindromeCheck.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class PalindromeCheck {
+
+    public static boolean isPalindrome(String text) {
+        int start = 0, end = text.length() - 1;
+
+        while (start < end) {
+            if (text.charAt(start) != text.charAt(end)) return false;
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+
+        System.out.println(isPalindrome(text) ? "Palindrome" : "Not Palindrome");
+    }
+}
