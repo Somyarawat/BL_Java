2a0893529c7bd7bb2b0d59a5e44498658cf4359e
Somya Rawat <somyarawat2005@gmail.com>
2026-04-20 22:35:04 +0530

    Check two texts are anagram or not and display the result

--- core-programming/core-programming-intermediate/methods/level-2/UnitConverter1.java
+++ core-programming/core-programming-intermediate/methods/level-2/UnitConverter1.java
@@ -1,15 +1,32 @@
 import java.util.Scanner;
 
-class LeapYear {
+class UnitConverter1 {
 
-    public static boolean isLeap(int y) {
-        return (y >= 1582) && ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0));
+    public static double convertKmToMiles(double km) {
+        return km * 0.621371;
+    }
+
+    public static double convertMilesToKm(double miles) {
+        return miles * 1.60934;
+    }
+
+    public static double convertMetersToFeet(double meters) {
+        return meters * 3.28084;
+    }
+
+    public static double convertFeetToMeters(double feet) {
+        return feet * 0.3048;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int y = sc.nextInt();
 
-        System.out.println(isLeap(y) ? "Leap Year" : "Not Leap Year");
+        System.out.println("Enter value:");
+        double value = sc.nextDouble();
+
+        System.out.println("Kilometers to Miles = " + convertKmToMiles(value));
+        System.out.println("Miles to Kilometers = " + convertMilesToKm(value));
+        System.out.println("Meters to Feet = " + convertMetersToFeet(value));
+        System.out.println("Feet to Meters = " + convertFeetToMeters(value));
     }
 }
