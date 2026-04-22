53ffcce30ad8a33942142c81292ad4991a6463da
Somya Rawat <somyarawat2005@gmail.com>
2026-04-22 16:09:18 +0530

    Create a Person class with a copy constructor that clones another person's attributes

---
+++ object-oriented-programming/OOP-Basics/constructors/Person.java
@@ -0,0 +1,25 @@
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    Person(Person p) {
+        name = p.name;
+        age = p.age;
+    }
+
+    void display(){
+        System.out.println(name + " " + age);
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Umang", 25);
+        Person p2 = new Person(p1);
+        p1.display();
+        p2.display();
+    }
+}
