fab3fa044f27ec91d422f26482fb0cbf9d92ad55
Somya Rawat <somyarawat2005@gmail.com>
2026-05-27 11:02:06 +0530

    Implement bubble sort

---
+++ data-structure-and-algorithms/algorithms/sort-algo/BubbleSort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean s;
+        for (int i = 0; i < n - 1; i++) {
+            s = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    s = true;
+                }
+            }
+            if (!s) break;
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] data = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Original array: " + Arrays.toString(data));
+        
+        bubbleSort(data);
+        
+        System.out.println("Sorted array:   " + Arrays.toString(data));
+    }
+}
