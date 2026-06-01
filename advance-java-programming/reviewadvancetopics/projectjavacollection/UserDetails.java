816c13cb745745f313cceee52012bd3aeaebbe21
Somya Rawat <somyarawat2005@gmail.com>
2026-06-01 22:53:44 +0530

    Store the user details in the user details file

---
+++ advance-java-programming/reviewadvancetopics/projectjavacollection/UserDetails.java
@@ -0,0 +1,15 @@
+
+public class UserDetails{
+        String name;
+        String address;
+        String phoneNo;
+
+        UserDetails(String name, String address, String phoneNo){
+            this.name = name;
+            this.address = address;
+            this.phoneNo = phoneNo;
+        }
+}
+
+
+
