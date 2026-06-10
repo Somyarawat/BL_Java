b19f8989fcb3fcaeafa1dc9affc8e7c553d47370
Somyarawat <157365756+Somyarawat@users.noreply.github.com>
2026-06-10 15:48:03 +0530

    Delete core-programming directory

--- core-programming/core-programming-basic/programming-elements/level-1/AverageMarks.java
+++
@@ -1,9 +0,0 @@
-class AverageMarks {
-    public static void main(String[] args) {
-        int maths = 96, physics = 94, chemistry = 92;
-
-        double average = (maths + physics + chemistry) / 3.0;
-
-        System.out.println("Sam's average marks in PCM is " + average);
-    }
-}
