3c4a30261441c086397c560ef8946c77feca4c3f
Somyarawat <157365756+Somyarawat@users.noreply.github.com>
2026-06-10 15:47:49 +0530

    Delete advance-java-programming directory

--- advance-java-programming/java-collections-and-streams/generics/bounded-types/MultipleBound.java
+++
@@ -1,8 +0,0 @@
-class MultipleBound{
-    public static<T extends Number & Comparable<T>> T findMax(T x, T y){
-        return (x.compareTo(y) > 0) ? x : y;
-    }
-    public static void main(String[] args) {
-        System.out.println(findMax(10, 20));
-    }
-}
